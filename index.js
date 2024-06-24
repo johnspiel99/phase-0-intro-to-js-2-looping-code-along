03a79cf9c4c4dc05b08d454b26fadb96d8e6d1e8
Johnspiel <njuejohnspiel@gmail.com>
2024-06-24 09:20:07 +0300

    New

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    let thankYouMessages = [];
+    for (let i = 0; i < namesArray.length; i++) {
+        thankYouMessages.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouMessages;
+}
+
+function countDown(startingNumber) {
+    let count = startingNumber;
+    while (count >= 0) {
+        console.log(count);
+        count--;
+    }
+}
